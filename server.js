5fa5fb10d1ad388922058c16e92ca89f44547fca
Emily Caton <caton.emily@yahoo.com>
2023-08-31 14:58:15 -0500

    troubleshooting error

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
+const fs = require('fs');
 const path = require("path");
 const {v4: uuidv4} = require("uuid"); //generate unique ids
 const app = express();
 const PORT = process.env.PORT || 3001;
+const cors = require('cors');
 
 //middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
-
+app.use(cors());
 //routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
@@ -16,12 +18,18 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) => {
+app.get('/notes', (req, res) => {
     const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')));
     res.json(notesData);
   });
 
-  app.post('/api/notes', (req, res) => {
+//   app.get("/api/notes", function(req, res) {
+//     let db = fs.readFileSync("./db/db.json", "utf8");
+//     db = JSON.parse(db);
+//     res.json(db);
+// });
+
+  app.post('/notes', (req, res) => {
     const newNote = req.body;
     newNote.id = uuidv4(); // Generate a unique ID for the new note
     const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')));
