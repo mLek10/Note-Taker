f37bdba9732f516ed9f140ca77d9700378d72aa0
Emily Caton <caton.emily@yahoo.com>
2023-08-31 22:50:50 -0500

    fix GET error

--- server.js
+++ server.js
@@ -4,32 +4,32 @@ const path = require("path");
 const {v4: uuidv4} = require("uuid"); //generate unique ids
 const app = express();
 const PORT = process.env.PORT || 3001;
-const cors = require('cors');
+// const notesData = JSON.parse(fs.readFileSync('db.json', 'utf-8'));
 
 //middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(cors());
+
 //routes
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+  res.sendFile(path.join(__dirname, 'public','notes.html'))
 );
+app.get('/api/notes', (req, res) => {
+  const notesData = fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8');
+  const parsedData = JSON.parse(notesData);
+  res.json(parsedData);
+});
 
-app.get('/notes', (req, res) => {
-    const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')));
-    res.json(notesData);
-  });
-
-
-  app.post('/notes', (req, res) => {
+  app.post('/api/notes', (req, res) => {
     const newNote = req.body;
     newNote.id = uuidv4(); // Generate a unique ID for the new note
-    const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')));
+    const filePath = path.join(__dirname, '\db.json');
+    const notesData = JSON.parse(fs.readFileSync(filePath));
     notesData.push(newNote);
-    fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notesData));
+    fs.writeFileSync(path.join(__dirname, '\db.json'), JSON.stringify(notesData));
     res.json(newNote);
   });
 
